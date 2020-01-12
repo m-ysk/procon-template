54e3bb94e244043141440d6e7f1ef701deffcc49
mysk <wolfbot.owner@gmail.com>
2020-01-12 19:08:13 +0900

    feat(main): make main template

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+#define rep(i, begin, n) for (int i = begin; i < n; i++)
+#define repe(i, begin, n) for (int i = begin; i <= n; i++)
+#define repr(i, begin, n) for (int i = begin; i > begin - n; i--)
+#define repre(i, begin, end) for (int i = begin; i >= end; i--)
+
+const int inf = 1000000007;
+const int MOD = 1000000007;
+const long long INF = 1000000000000000007;
+
+int main()
+{
+}
