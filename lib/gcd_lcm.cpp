0b26cf783d7fd5bf230fe3d3d766ca4a3d29472e
mysk <wolfbot.owner@gmail.com>
2020-06-10 23:23:29 +0900

    fix(lcm): prevent overflow in lcm

--- lib/gcd_lcm.cpp
+++ lib/gcd_lcm.cpp
@@ -6,21 +6,15 @@ const int inf = 1000000007;
 const int MOD = 1000000007;
 const long long INF = 1000000000000000007;
 
-int gcd(int a, int b)
-{
-    if (b == 0)
-        return a;
+long long gcd(long long a, long long b) {
+  if (b == 0) return a;
 
-    return gcd(b, a % b);
+  return gcd(b, a % b);
 }
 
-int lcm(int a, int b)
-{
-    return a * b / gcd(a, b);
-}
+long long lcm(long long a, long long b) { return a / gcd(a, b) * b; }
 
-int main()
-{
-    cout << gcd(120, 115) << endl;
-    cout << lcm(15, 20) << endl;
+int main() {
+  cout << gcd(120, 115) << endl;
+  cout << lcm(15, 20) << endl;
 }
