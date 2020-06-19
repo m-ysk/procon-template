ae05f0f129b8e6c0edcd2b0a5159f19ce88c3b0c
mysk <wolfbot.owner@gmail.com>
2020-06-19 22:33:09 +0900

    feat(main): add Pii and Pll

--- main.cpp
+++ main.cpp
@@ -1,31 +1,29 @@
 #include "bits/stdc++.h"
 using namespace std;
 using ll = long long;
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
 #define rep(i, begin, n) for (int i = begin; i < n; i++)
 #define repe(i, begin, n) for (int i = begin; i <= n; i++)
 #define repr(i, begin, n) for (int i = begin; i > begin - n; i--)
 #define repre(i, begin, end) for (int i = begin; i >= end; i--)
 
 template <class T>
-inline bool chmax(T &a, T b)
-{
-    if (a < b)
-    {
-        a = b;
-        return 1;
-    }
-    return 0;
+inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
 }
 
 template <class T>
-inline bool chmin(T &a, T b)
-{
-    if (a > b)
-    {
-        a = b;
-        return 1;
-    }
-    return 0;
+inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
 }
 
 const int inf = 1000000007;
@@ -34,6 +32,4 @@ const long long INF = 1000000000000000007;
 
 // -------------------------------------------------------
 
-int main()
-{
-}
+int main() {}
